af7dd54c084c9da5982b1bbd89dcc3cea4f99576
-WILEX- <yracnet@gmail.com>
2014-04-24 09:48:07 -0700

    Create RobertParamException.java

---
+++ src/hsk/wyujra/RobertParamException.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hsk.wyujra;
+
+/**
+ *
+ * @author Developer
+ */
+public class RobertParamException extends Exception{
+
+    public RobertParamException(String m) {
+        super(m);
+    }
+
+    public RobertParamException(String m, Throwable e) {
+        super(m, e);
+    }
+    
+}
